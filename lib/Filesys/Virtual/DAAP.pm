780298170a1c8eb3f2546dddf5b296a35ffe774c
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-05-07 11:09:13 +0000

    and soon we shall take over the earth!
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Filesys-Virtual-DAAP@2437 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Filesys/Virtual/DAAP.pm
@@ -0,0 +1,51 @@
+package Filesys::Virtual::DAAP;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Filesys::Virtual::DAAP - 
+
+=head1 SYNOPSIS
+
+  use Filesys::Virtual::DAAP;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright 2004 Richard Clamp.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Filesys::Virtual::DAAP>.
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
